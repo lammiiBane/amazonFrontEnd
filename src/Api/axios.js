84033489ba0f3f77a09eddab9956e6eb71a68410
lemegudata@gmail.com <146845168+Lamibane@users.noreply.github.com>
2024-02-16 12:49:29 +0300

    first commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  // baseURL: "http://127.0.0.1:5001/clone-38286/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-50ee4/us-central1/api",
   // deployed version of firebase function
-  baseURL: "https://api-abyjie4qeq-uc.a.run.app",
+  // baseURL: "https://api-abyjie4qeq-uc.a.run.app",
 
   // deployed version of amazon server on render.com
-  // baseURL: "https://amazon-api-deploy.onrender.com",
+  baseURL: "https://amazonapi-fr69.onrender.com",
 });
 
 export { axiosInstance };
--- src/Router.jsx
+++ src/Router.jsx
@@ -17,7 +17,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 const stripePromise = loadStripe(
-  "pk_test_51OU6YoIrgvX7vjwlwFwZFFmBbuXRo77ewG49UZBndLITKEtINgOsTCmKbx4dvva5opJQYlTggPJ3SuK3HpprL1Pj006REMjwGf"
+  "pk_test_51OkAwZD5VLPFVmRCpGNFpTZplksa5D20G9K7c8c8E6P2LaRrMRQfsR6zsXlmmGWT5MvPPyiC37tv9qI7QK7duZp100VGysYYmu"
 );
 function Routing() {
   return (
